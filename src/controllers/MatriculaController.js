e3d743d10c3b2a47358636dae88ced913ba4cef7
wallacevieira <wallaceofc@hotmail.com>
2024-05-28 13:41:23 -0300

    feat: transactions

--- src/controllers/MatriculaController.js
+++ src/controllers/MatriculaController.js
@@ -12,38 +12,38 @@ class MatriculaController extends Controller {
 
   async pegaMatriculasPorEstudante(req, res) {
     const { estudante_id } = req.params;
-
     try {
-      const listaMatriculasPorEstudante = await matriculaServices.pegaEContaRegistros({
-        where : {
-          estudante_id: Number(estudante_id),
-          status: 'matriculado'
-        },
-        limit: 2,
-        order: [['id', 'ASC']]
-      });
-
+      const listaMatriculasPorEstudante = await matriculaServices.pegaEContaRegistros(
+        {
+          where: {
+            estudante_id: Number(estudante_id),
+            status: 'matriculado'
+          },
+          limit: 2,
+          order: [['id', 'DESC']]
+        }
+      );
       return res.status(200).json(listaMatriculasPorEstudante);
     } catch (erro) {
-      res.status(500).json({ erro: erro.message });
+      return res.status(500).json({ erro: erro.message });
     }
   }
 
   async pegaCursosLotados (req, res) {
     const lotacaoCurso = 2;
     try {
-      const cursosLotados = await matriculaServices.pegaEContaRegistros({
-        where: {
-          status: 'matriculado',
-        },
-        attributes: ['curso_id'],
-        group: ['curso_id'],
-        having: Sequelize.literal(`count(curso_id) >= ${lotacaoCurso}`)
-      });
-
+      const cursosLotados = await matriculaServices.pegaEContaRegistros(
+        {
+          where: {
+            status: 'matriculado'
+          },
+          attributes: ['curso_id'],
+          group: ['curso_id'],
+          having: Sequelize.literal(`count(curso_id) >= ${lotacaoCurso}`)
+        });
       return res.status(200).json(cursosLotados.count);
     } catch (erro) {
-      res.status(500).json({ erro: erro.message });
+      return res.status(500).json({ erro: erro.message });
     }
   }
 }
--- src/services/Services.js
+++ src/services/Services.js
@@ -6,10 +6,10 @@ class Services {
   }
 
   async pegaTodosOsRegistros (where = {}) {
-    return dataSource[this.model].findAll({ where: { ...where } });
+    return dataSource[this.model].findAll({ where: { ...where }});
   }
 
-  async pegaRegistrosPorEscopo(escopo) {
+  async pegaRegistrosPorEscopo (escopo) {
     return dataSource[this.model].scope(escopo).findAll();
   }
 
@@ -29,10 +29,12 @@ class Services {
     return dataSource[this.model].create(dadosDoRegistro);
   }
 
-  async atualizaRegistro(dadosAtualizados, where) {
-    const listadeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, {
-      where: { ...where }
-    });
+  async atualizaRegistro(dadosAtualizados, where, transacao = {}) {
+    const listadeRegistrosAtualizados = await dataSource[this.model]
+      .update(dadosAtualizados, {
+        where: { ...where },
+        transaction: transacao
+      });
     if (listadeRegistrosAtualizados[0] === 0) {
       return false;
     }
